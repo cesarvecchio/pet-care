17fa228fad4e6afc5b83401aa6e8d74810bf20c3
cesarvecchio <ozaro.cesar@gmail.com>
2023-10-17 19:16:38 -0300

    Implementação da repository de Dono

--- src/main/java/br/com/petcare/infra/repositorio/DonoRepository.java
+++ src/main/java/br/com/petcare/infra/repositorio/DonoRepository.java
@@ -1,2 +1,9 @@
-package br.com.petcare.infra.repositorio;public interface DonoRepository {
+package br.com.petcare.infra.repositorio;
+
+import br.com.petcare.dominio.entidade.Dono;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DonoRepository extends JpaRepository<Dono, Integer> {
 }
