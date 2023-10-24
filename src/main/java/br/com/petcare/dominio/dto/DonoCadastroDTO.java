ae879807856a03c6ca04d4c56515cf1aea3517e1
João Marcos <jmasjesus@protonmail.com>
2023-10-23 21:50:10 -0300

    Adicionando embedded de endereco em dono

--- src/main/java/br/com/petcare/dominio/dto/DonoCadastroDTO.java
+++ src/main/java/br/com/petcare/dominio/dto/DonoCadastroDTO.java
@@ -1,11 +1,15 @@
 package br.com.petcare.dominio.dto;
 
+import br.com.petcare.dominio.entidade.Endereco;
+
 public record DonoCadastroDTO(
         Integer id,
         String nome,
         String cpf,
         String rg,
         String email,
-        String senha
+        String senha,
+
+        Endereco endereco
 ) {
 }
