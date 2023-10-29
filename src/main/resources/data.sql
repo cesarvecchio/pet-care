e87d0b663a57325891dafbaac7bf4997230a832f
cesarvecchio <ozaro.cesar@gmail.com>
2023-10-29 12:58:52 -0300

    Remoção do set de ID das tabelas de Usurio

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO TB_USUARIO (ID, NOME, EMAIL, SENHA, CEP, LOGRADOURO, COMPLEMENTO, BAIRRO, CIDADE, UF, NUMERO, LATITUDE,
+INSERT INTO TB_USUARIO (NOME, EMAIL, SENHA, CEP, LOGRADOURO, COMPLEMENTO, BAIRRO, CIDADE, UF, NUMERO, LATITUDE,
                         LONGITUDE)
-VALUES (1, 'Grupo', 'grupo@gmail.com', 'grupo', '01310930', 'Av Paulista', 'Ao Lado do Banco Itaú', 'Bela Vista',
+VALUES ('Grupo', 'grupo@gmail.com', 'grupo', '01310930', 'Av Paulista', 'Ao Lado do Banco Itaú', 'Bela Vista',
         'São Paulo', 'SP', '1000', -23.562372,
         -46.653939);
 
@@ -10,9 +10,9 @@ VALUES (1, '12313131313', '23323322');
 INSERT INTO tb_pet (nome, especie, raca, humor, genero, dono_id)
 VALUES ('Pet do grupo', 1, 4, 1, 2, 1);
 
-INSERT INTO TB_USUARIO (ID, NOME, EMAIL, SENHA, CEP, LOGRADOURO, COMPLEMENTO, BAIRRO, CIDADE, UF, NUMERO, LATITUDE,
+INSERT INTO TB_USUARIO (NOME, EMAIL, SENHA, CEP, LOGRADOURO, COMPLEMENTO, BAIRRO, CIDADE, UF, NUMERO, LATITUDE,
                         LONGITUDE)
-VALUES (2, 'PET LOVE', 'admin@petlove.com', 'petlovepasswd', '02525050', 'Avenida Braz Leme', 'Lojas 16 e 17', 'Santana', 'São Paulo', 'SP', '1200',
+VALUES ('PET LOVE', 'admin@petlove.com', 'petlovepasswd', '02525050', 'Avenida Braz Leme', 'Lojas 16 e 17', 'Santana', 'São Paulo', 'SP', '1200',
         -23.507549, -46.648546);
 
 INSERT INTO TB_PET_SHOP (ID, CNPJ, TIPO_SERVICO)
@@ -21,5 +21,5 @@ VALUES (2, '10864846000123', 1);
 INSERT INTO TB_FUNCIONARIO (NOME, CPF, RG, PET_SHOP_ID)
 VALUES ('Kleber Machado', '12378919426', '768945803', 2);
 
-INSERT INTO TB_AGENDAMENTO (ID, STATUS, DATA_AGENDAMENTO, OBSERVACAO, PET_ID, PET_SHOP_ID, DONO_ID)
-VALUES(1, 1, '2021-06-20 10:00:00', 'Pet do grupo', 1, 2, 1);
+INSERT INTO TB_AGENDAMENTO (STATUS, DATA_AGENDAMENTO, OBSERVACAO, PET_ID, PET_SHOP_ID, DONO_ID)
+VALUES(1, '2021-06-20 10:00:00', 'Pet do grupo', 1, 2, 1);
